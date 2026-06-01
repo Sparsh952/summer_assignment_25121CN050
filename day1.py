deabd9103eb665b55bf57d63ae941ec0ac6e3f85
Sparsh952 <sparshkumar8888@gmail.com>
2026-06-01 22:03:00 +0530

    day 1 uploaded

--- day1.py
+++ day1.py
@@ -1,45 +1,45 @@
-# # #Write a program to Calculate sum of first N natural numbers
+#Write a program to Calculate sum of first N natural numbers
 
 
-# # n = int(input("Enter a positive integer: "))
-# # if n < 1:
-# #     print("Please enter a valid natural number (1 or greater).")
-# # else:
+n = int(input("Enter a positive integer: "))
+if n < 1:
+    print("Please enter a valid natural number (1 or greater).")
+else:
     
-# #     total_sum = (n * (n + 1)) // 2
+    total_sum = (n * (n + 1)) // 2
     
-# #     print(f"The sum of the first {n} natural numbers is: {total_sum}")
+    print(f"The sum of the first {n} natural numbers is: {total_sum}")
 
 
 
-#     #Write a program to Print multiplication table of a given number
+    #Write a program to Print multiplication table of a given number
 
-# num = int(input("Enter a number to print its multiplication table: "))
+num = int(input("Enter a number to print its multiplication table: "))
 
-# for i in range(1, 11):
-#     print(f"{num} x {i} = {num * i}")
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
 
 
 
-# #Write a program to Find factorial of a number
+#Write a program to Find factorial of a number
 
-# num = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
 
-# factorial = 1
+factorial = 1
 
-# if num < 0:
-#     print("Factorial does not exist for negative numbers.")
-# elif num == 0:
-#     print("The factorial of 0 is 1.")
-# else:
+if num < 0:
+    print("Factorial does not exist for negative numbers.")
+elif num == 0:
+    print("The factorial of 0 is 1.")
+else:
     
-#     for i in range(1, num + 1):
-#         factorial *= i
-#     print(f"The factorial of {num} is {factorial}")
+    for i in range(1, num + 1):
+        factorial *= i
+    print(f"The factorial of {num} is {factorial}")
 
 
 
-    #Write a program to Count digits in a number. 
+   # Write a program to Count digits in a number. 
 
 
 num = int(input("Enter a number: "))
